f3b51e054ebce44b6dff23258d74218b88dac21e
kanzaki <zkanzaki@vsa.co.jp>
2011-10-04 21:53:32 +0900

    Edited KANAME_Check/src/Main.scala via GitHub

--- KANAME_Check/src/Main.scala
+++ KANAME_Check/src/Main.scala
@@ -2,7 +2,7 @@ object Main {
 	import db._
   
 	def main(args: Array[String]): Unit = { 
-			
+・・＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
 	    var t = new Tester
 	    t.first()
 	    t.nest()
